9267d650bce5e4aa86701d2b8993dc26f6223a40
S566474 <142925730+S566474@users.noreply.github.com>
2023-10-25 14:49:04 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Sai Swathi Thota' });
+  res.render('mydata', { title: 'Swathi Thota' });
 });
 
 module.exports = router;
