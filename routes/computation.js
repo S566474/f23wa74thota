9974f37cd9628b7117c33cb2bb66df8a770ec7fb
S566474 <142925730+S566474@users.noreply.github.com>
2023-10-25 21:46:50 -0500

    bonus added

---
+++ routes/computation.js
@@ -0,0 +1,23 @@
+
+
+var express = require('express');
+var router = express.Router();
+
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    x=req.query.x;
+    y=req.query.y;
+    if(x==undefined && y==undefined)
+    x=(Math.random()*100);
+    y=(Math.random()*100);
+
+    hyp=Math.hypot(x,y);
+    ce=Math.ceil(x);
+    cl=Math.clz32(x);
+    res.send(`hypot of ${x,y} is ${hyp},ceil of ${x} is ${ce} and clz32 of ${x} is ${cl}`);
+  
+});
+
+module.exports = router;
